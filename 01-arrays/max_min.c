567efcdf3c490dc7c77380ced6d8fa54ac4f3a8d
Shivam Sharma <godshivam12007@gmail.com>
2026-01-31 10:30:25 +0530

    Update max_min.c

--- 01-arrays/max_min.c
+++ 01-arrays/max_min.c
@@ -1,31 +1,38 @@
+/*
+Question:
+Given an array of integers, find the maximum and minimum elements
+present in the array using a single traversal.
+*/
+
 #include <stdio.h>
+
 void main() {
     int size;
+
     printf("Enter size of the array: ");
-    scanf("%d",&size);
-    
+    scanf("%d", &size);
+
     int arr[size];
-    
-    printf("enter elements: ");
-    for (int i=0;i<=size-1;i++) {
-        scanf("%d",&arr[i]);
+
+    printf("Enter elements:\n");
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
     }
-    
-    int min=arr[0],max=arr[0];
-    
-    for (int i=0;i<=size-1;i++) {
-        if (min >= arr[i]) {
+
+    int min = arr[0];
+    int max = arr[0];
+
+    for (int i = 1; i < size; i++) {
+        if (arr[i] < min) {
             min = arr[i];
         }
-        if (max <= arr[i]) {
+        if (arr[i] > max) {
             max = arr[i];
         }
-        
     }
-    printf("\nthis is the array: ");
-    for (int i=0;i<=size-1;i++) {
-        printf("%d\t",arr[i]);
-    }
-    printf("\nmax: %d, min: %d",max,min);
-    
+
+    printf("Maximum element: %d\n", max);
+    printf("Minimum element: %d\n", min);
+
 }
+
